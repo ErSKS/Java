187d73326ed58e1d7599a280a664169cdb0c730a
Er. Shiva K. Shrestha <shrestha.shiva@hotmail.com>
2017-07-27 07:46:20 +0545

    Add files via upload

---
+++ Pattern1.java
@@ -0,0 +1,21 @@
+package pattern1;
+
+/**
+ *
+ * @author ErSKS
+ */
+public class Pattern1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+
+}

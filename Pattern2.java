e1b7330b8fab054e6d3f3d8ef56e17605f35b9ae
Er. Shiva K. Shrestha <shrestha.shiva@hotmail.com>
2017-07-27 08:16:15 +0545

    Pattern2

---
+++ Pattern2.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pattern2;
+
+/**
+ *
+ * @author ErSKS
+ */
+public class Pattern2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = i; j < 5; j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+
+}
